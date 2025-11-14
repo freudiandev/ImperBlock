5a1493681e82d5740a6b75b3ebca811a6b9c48eb
freudianDev <alexalmeida@riseup.net>
2025-11-14 08:47:59 -0500

    deploy: export limpio desde main v.0.0.008

---
+++ out/_next/static/fnJMLD9uDtXdHE_Cvtyw3/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/f27a7fb3cacf5728.js"
+  ],
+  "/_error": [
+    "static/chunks/1c8c077c1e040c75.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
