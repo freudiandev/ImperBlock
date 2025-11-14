b0ac1c3fc97c37916702f0ea02c58a23c8a92539
freudianDev <alexalmeida@riseup.net>
2025-11-13 23:25:35 -0500

    v.0.0.21

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: { unoptimized: true },
   trailingSlash: true,
   output: 'export',
-  basePath: '/ImperBlock',
-  assetPrefix: '/ImperBlock/',
   // ...otros settings si tienes
 };
 module.exports = nextConfig
