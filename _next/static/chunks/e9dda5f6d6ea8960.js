f74b2ac1e06a5a8e890e39d14f6a4d89c230b5d5
freudianDev <alexalmeida@riseup.net>
2025-11-14 01:31:48 -0500

    Despliegue: actualiza archivos estáticos exportados

---
+++ _next/static/chunks/e9dda5f6d6ea8960.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/9576563fe03c8625.js",
+  "static/chunks/a295171739d10e16.js",
+  "static/chunks/f7e1b5f19806c1b0.css",
+  "static/chunks/turbopack-ae6e1d0c43c36be8.js"
+])
---
+++ _next/static/chunks/f27a7fb3cacf5728.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/6f37acb95578695d.js",
+  "static/chunks/a295171739d10e16.js",
+  "static/chunks/turbopack-46fdc3c69d586f3e.js"
+])
